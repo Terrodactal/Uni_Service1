024c0b6d8d71dac6b98a0825c6476068a01ab906
Terrodactal <virakyuthchoup@gmail.com>
2026-09-17 16:47:18 +0700

    Update login-service.js

--- Login_microservice/login-service.js
+++ Login_microservice/login-service.js
@@ -56,6 +56,6 @@ app.post("/login", async (req, res) => {
     }
 });
 
-app.listen(44.192.27.186:5002, () => {
+app.listen(5002, () => {
     console.log('Authentication Service Server is running on PORT NO: 44.192.27.186:5002');
 });
