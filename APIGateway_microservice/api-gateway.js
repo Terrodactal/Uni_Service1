1e8e5a99808aafcae45933785b155fd758b86f0e
Terrodactal <virakyuthchoup@gmail.com>
2026-09-17 16:42:55 +0700

    Update api-gateway.js

--- APIGateway_microservice/api-gateway.js
+++ APIGateway_microservice/api-gateway.js
@@ -46,12 +46,12 @@ app.use('/admin', authToken, authRole('admin'),(req, res) => {
 
 //REDIRECT TO THE LOGIN(Authentication) MICROSERVICE
 app.use('/login', (req, res) => {
-    proxy.web(req, res, { target: 'http://44.192.27.186:5002' });
+    proxy.web(req, res, { target: 'http://localhost:5002' });
 })
 
 //REDIRECT TO THE LOGIN(Authentication) MICROSERVICE
 app.use('/registration', (req, res) => {
-    proxy.web(req, res, { target: 'http://44.192.27.186:5001' });
+    proxy.web(req, res, { target: 'http://localhost:5001' });
 })
 
 app.listen(44.192.27.186:4000, () => {
